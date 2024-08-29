d75c3bea250df4cf501122b7d9e1268ddbe592a4
Deepak <deepaksai9391@gmail.com>
2024-08-29 21:09:40 +0530

    initial commit

---
+++ src/Hero.jsx
@@ -0,0 +1,26 @@
+const HeroSection = () => {
+    return(
+    <main className="hero container">
+        <div className="hero-content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam praesentium alias voluptates iusto sed excepturi aliquam vitae distinctio, reprehenderit atque sapiente accusantium error officiis ut ratione ad quos repudiandae qui!
+                </p>
+        <div className="hero-btn">
+            <button className="button-2">Shop Now</button>
+            <button className="second-btn">Category</button>
+        </div>
+        <div className="shopping">
+            <p>Available on</p>
+            <div className="brand-icons">
+                <img src=".\public\images\amazon.png" alt="logo" />
+                <img src=".\public\images\flipkart.png" alt="logo" />
+            </div>
+        </div>
+        </div>
+        <div className="hero-image">
+            <img src=".\public\images\shoe_image2.png" alt="shoe" />
+        </div>
+    </main>
+    );
+};
+export default HeroSection;
---
+++ src/Nav.jsx
@@ -0,0 +1,20 @@
+const Navigation = () => {
+    return(
+        
+        <nav className="container">
+          <div className="logo">
+            <img src=".\public\images\pumalogo.png" alt="logo" />
+  
+          </div>
+          <ul>
+            <li href="#">Menu</li>
+            <li href="#">Location</li>
+            <li href="#">About</li>
+            <li href="#">Conatact</li>
+          </ul>
+          <button className="button">login</button>
+        </nav>
+      
+    );
+};
+export default Navigation;
